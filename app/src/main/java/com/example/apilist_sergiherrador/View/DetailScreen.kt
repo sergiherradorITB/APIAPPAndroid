64439d6f1af35554c34d4a2aab4b1ba421413ee9
sergiherradorITB <sergi.herrador.7e7@itb.cat>
2024-02-09 08:03:38 +0100

    API Images working

--- app/src/main/java/com/example/apilist_sergiherrador/View/DetailScreen.kt
+++ app/src/main/java/com/example/apilist_sergiherrador/View/DetailScreen.kt
@@ -1,12 +1,16 @@
 package com.example.apilist_sergiherrador.View
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,34 +22,38 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.apilist_sergiherrador.Model.DataItem
 import com.example.apilist_sergiherrador.R
+import com.example.apilist_sergiherrador.Routes
 import com.example.apilist_sergiherrador.ViewModel.ListScreenViewModel
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun DetailScreen(navController: NavController, listScreenViewModel: ListScreenViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        // verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // Imagen arriba
-        Image(
-            painter = painterResource(id = R.drawable.castillo_ambulante),
-            contentDescription = null,
+        GlideImage(
+            model = listScreenViewModel.ghibli.image,
+            contentDescription = "Character Image",
+            contentScale = ContentScale.Fit,
             modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp),
-            contentScale = ContentScale.Crop
+                .fillMaxWidth(1f)
+                .fillMaxHeight(0.4f)
         )
-
 
         // Título grande
         Text(
             text = listScreenViewModel.ghibli.title,
             modifier = Modifier.fillMaxWidth(),
         )
-
 
         // Descripción
         Text(
@@ -54,5 +62,11 @@ fun DetailScreen(navController: NavController, listScreenViewModel: ListScreenVi
             maxLines = 5,
             overflow = TextOverflow.Ellipsis
         )
+
+        Button(onClick = {
+            navController.navigate(Routes.ListScreen.route)
+        }) {
+            Text(text = "Volver al menú")
+        }
     }
 }
--- app/src/main/java/com/example/apilist_sergiherrador/View/ListScreen.kt
+++ app/src/main/java/com/example/apilist_sergiherrador/View/ListScreen.kt
@@ -1,7 +1,17 @@
 package com.example.apilist_sergiherrador.View
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -14,11 +24,17 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.apilist_sergiherrador.Model.DataItem
+import com.example.apilist_sergiherrador.R
 import com.example.apilist_sergiherrador.Routes
 import com.example.apilist_sergiherrador.ViewModel.APIViewModel
 import com.example.apilist_sergiherrador.ViewModel.ListScreenViewModel
@@ -26,71 +42,113 @@ import com.example.apilist_sergiherrador.ViewModel.ListScreenViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ListScreen(navigationController: NavController, apiViewModel: APIViewModel, listScreenViewModel: ListScreenViewModel) {
+fun ListScreen(
+    navigationController: NavController,
+    apiViewModel: APIViewModel,
+    listScreenViewModel: ListScreenViewModel
+) {
     val showLoading: Boolean by apiViewModel.loading.observeAsState(true)
     val characters: List<DataItem> by apiViewModel.films.observeAsState(emptyList<DataItem>())
     val searchText: String by apiViewModel.searchText.observeAsState("")
     apiViewModel.getCharacters()
-
     if (showLoading) {
         CircularProgressIndicator(
             modifier = Modifier.width(64.dp),
             color = MaterialTheme.colorScheme.secondary
         )
     } else {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth(1f)
-                .fillMaxHeight(1f)
-        ) {
-            TextField(
-                value = searchText,
-                onValueChange = { apiViewModel.onSearchTextChange(it) },
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.studioghibli),
+                contentDescription = "logo",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                label = { Text(text = "Search") }
+                    .fillMaxHeight(0.3f)
+                    .padding(16.dp)
             )
-
-            LazyColumn(
-                modifier = Modifier.fillMaxHeight(0.9f),
-                verticalArrangement = Arrangement.spacedBy(4.dp)
-            ) {
-                val filteredCharacters = characters.filter { ghibli ->
-                    ghibli.title.contains(searchText) || ghibli.original_title.contains(searchText)
-                }
-                items(filteredCharacters) { ghibli ->
-                    GhibliItem(ghibli = ghibli, navController = navigationController, listScreenViewModel)
-                }
-            }
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+                    .fillMaxWidth(1f)
+                    .fillMaxHeight(1f)
             ) {
-                Text(text = "Favs")
-                Text(text = "All")
-            }
-            Button(onClick = {                 navigationController.navigate(Routes.DetailScreen.route)
-            }) {
-            Text(text = "PERU")
+                TextField(
+                    value = searchText,
+                    onValueChange = { apiViewModel.onSearchTextChange(it) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    label = { Text(text = "Search") }
+                )
+
+                LazyColumn(
+                    modifier = Modifier.fillMaxHeight(0.9f),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    val filteredCharacters = characters.filter { ghibli ->
+                        ghibli.title.contains(searchText) || ghibli.original_title.contains(
+                            searchText
+                        )
+                    }
+                    items(filteredCharacters) { ghibli ->
+                        GhibliItem(
+                            ghibli = ghibli,
+                            navController = navigationController,
+                            listScreenViewModel
+                        )
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Favs")
+                    Text(text = "All")
+                }
+                Button(onClick = {
+                    navigationController.navigate(Routes.DetailScreen.route)
+                }) {
+                    Text(text = "PERU")
+                }
             }
         }
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-fun GhibliItem(ghibli: DataItem, navController: NavController, listScreenViewModel: ListScreenViewModel) {
+fun GhibliItem(
+    ghibli: DataItem,
+    navController: NavController,
+    listScreenViewModel: ListScreenViewModel
+) {
     Card(border = BorderStroke(2.dp, Color.LightGray), modifier = Modifier.fillMaxWidth()) {
         Column {
-            Text(text = ghibli.title)
-            Text(text = ghibli.original_title)
-            Button(onClick = {
-                listScreenViewModel.modificarGhibli(ghibli)
-                navController.navigate(Routes.DetailScreen.route)
-            }) {
-                Text(text = "Ver detalles")
+            Row {
+                // Imagen arriba
+                GlideImage(
+                    model = ghibli.image,
+                    contentDescription = "Character Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(100.dp)
+                )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    // verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = ghibli.title)
+                    Text(text = ghibli.original_title)
+
+                    Button(onClick = {
+                        listScreenViewModel.modificarGhibli(ghibli)
+                        navController.navigate(Routes.DetailScreen.route)
+                    }) {
+                        Text(text = "Ver detalles")
+                    }
+                }
+
             }
+
         }
     }
 }
